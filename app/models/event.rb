9800efa178d3c9e014cacfc1e107cf8f2c58f72e
juanraba <jrabanillog@gmail.com>
2018-12-26 19:13:44 -0300

    add method to_json little useless as fields are equal

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+  def start2
+    start    
+  end
+
+  def to_json(options = {})
+    options[:except] = [:start2, :created_at]
+    options[:methods] = :start
+    super
+  end
 end
