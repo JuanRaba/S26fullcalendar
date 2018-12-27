a1c204152b16dfc60f2ef823eee06d3a772fdcee
juanraba <jrabanillog@gmail.com>
2018-12-27 13:06:26 -0300

    eventos por ajax

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,10 @@ class EventsController < ApplicationController
   # GET /events.json
   def index
     @events = Event.all
+    # respond_to do |format|
+    #   format.html
+    #   format.json {render json: @events.to_json }
+    # end
   end
 
   # GET /events/1
